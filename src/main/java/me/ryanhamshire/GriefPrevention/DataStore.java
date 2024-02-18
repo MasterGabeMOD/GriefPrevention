70f98a20f33e7b42542603075491f88f3b1372c3
MasterGabeMOD <MasterGabeMod@yahoo.com>
2024-02-17 20:53:55 -0800

    Merge latest changes

--- src/main/java/me/ryanhamshire/GriefPrevention/DataStore.java
+++ src/main/java/me/ryanhamshire/GriefPrevention/DataStore.java
@@ -1549,6 +1549,9 @@ public abstract class DataStore
 
                 //show the player the conflicting claim
                 BoundaryVisualization.visualizeClaim(player, result.claim, VisualizationType.CONFLICT_ZONE);
+                
+              //log conflicting claim, if debug logs are enabled
+                GriefPrevention.AddLogEntry(player.getName() + " attempted to create a claim, but conflicted with claim " + result.claim.id, CustomLogEntryTypes.Debug);
             }
             else
             {
